f3f62a962006b97266c74066f5ebe9c4b41a17ea
Stuart Harris <stuart.harris@red-badger.com>
2021-10-21 20:46:32 +0100

    call upstream

--- provider/src/main.rs
+++ provider/src/main.rs
@@ -1,8 +1,13 @@
 use log::debug;
+use std::{convert::Infallible, sync::Arc};
+use tokio::sync::RwLock;
+use upstream::QueryResult;
 use wasmbus_rpc::provider::prelude::*;
 use wasmcloud_graphql_interface::{GraphQL, GraphQLReceiver, QueryRequest, QueryResponse};
 
 mod upstream;
+
+const DATABASE_URL_KEY: &str = "DATABASE_URL";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     provider_main(GraphQLProvider::default())?;
@@ -13,11 +18,54 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 #[derive(Default, Clone, Provider)]
 #[services(GraphQL)]
-struct GraphQLProvider {}
+struct GraphQLProvider {
+    instance: Arc<RwLock<Option<String>>>,
+}
 
 /// use default implementations of provider message handlers
 impl ProviderDispatch for GraphQLProvider {}
-impl ProviderHandler for GraphQLProvider {}
+#[async_trait]
+impl ProviderHandler for GraphQLProvider {
+    /// Provider should perform any operations needed for a new link,
+    /// including setting up per-actor resources, and checking authorization.
+    /// If the link is allowed, return true, otherwise return false to deny the link.
+    async fn put_link(&self, ld: &LinkDefinition) -> RpcResult<bool> {
+        let database_url = match ld.values.get(DATABASE_URL_KEY) {
+            Some(v) => Some(v.to_string()),
+            None => {
+                return Err(RpcError::InvalidParameter(format!(
+                    "{} must be set",
+                    DATABASE_URL_KEY
+                )))
+            }
+        };
+        let mut instance = self.instance.write().await;
+        if *instance != database_url {
+            return Err(RpcError::InvalidParameter(format!(
+				"instance already initialised with a different {}, and we currently only support one connection",
+				DATABASE_URL_KEY
+			)));
+        }
+        upstream::init("1");
+        *instance = database_url;
+        Ok(true)
+    }
+
+    /// Handle notification that a link is dropped - release the upstream
+    async fn delete_link(&self, _actor_id: &str) {
+        let mut instance = self.instance.write().await;
+        upstream::remove("1");
+        *instance = None;
+    }
+
+    /// Handle shutdown request by releasing upstream
+    async fn shutdown(&self) -> Result<(), Infallible> {
+        let mut instance = self.instance.write().await;
+        upstream::remove("1");
+        *instance = None;
+        Ok(())
+    }
+}
 
 /// Handle GraphQL methods
 #[async_trait]
@@ -25,6 +73,22 @@ impl GraphQL for GraphQLProvider {
     /// updates the text on the GraphQL display
     async fn query(&self, _ctx: &Context, req: &QueryRequest) -> RpcResult<QueryResponse> {
         debug!("processing request update({})", req.query);
-        Ok(QueryResponse::default())
+        match upstream::query(
+            "1",
+            r#"
+		query MyQuery {
+			productLists {
+				nodes {
+					nodeId
+					userId
+					id
+					title
+				}
+			}
+		}"#,
+        ) {
+            QueryResult::Ok(result) => Ok(QueryResponse { data: result }),
+            QueryResult::Err(err) => Err(RpcError::MethodNotHandled(err)),
+        }
     }
 }
--- provider/src/upstream.rs
+++ provider/src/upstream.rs
@@ -4,32 +4,22 @@ use nodejs::neon::{
     prelude::Handle,
     reflect::eval,
     result::{JsResult, NeonResult},
-    types::{JsError, JsFunction, JsString, JsUndefined, JsValue, Value},
+    types::{Finalize, JsBox, JsError, JsFunction, JsString, JsValue, Value},
 };
-use std::{sync::mpsc, thread, time::Duration};
+use std::sync::mpsc;
 
-pub fn start() {
-    require();
-    thread::sleep(Duration::from_millis(1000));
-    for _ in 1..=4 {
-        query(
-            r#"
-			query MyQuery {
-				productLists {
-					nodes {
-						nodeId
-						userId
-						id
-						title
-					}
-				}
-			}"#,
-        );
-    }
-    thread::park();
+#[derive(Clone)]
+pub enum QueryResult {
+    Ok(String),
+    Err(String),
 }
 
-fn require() {
+impl Finalize for QueryResult {}
+
+/// load (an instance of?) the PostGraphile middleware into node
+/// - not sure yet whether we can actually have isolated instances
+pub fn init(instance: &str) {
+    let instance = instance.to_owned();
     sync_node(move |mut cx| {
         let require: Handle<JsFunction> = cx
             .global()
@@ -38,47 +28,49 @@ fn require() {
         let undefined = cx.undefined();
         let module_path: Handle<JsString> = cx.string("./dist/src/index.js");
         let module = require.call(&mut cx, undefined, vec![module_path])?;
-        cx.global().set(&mut cx, "query", module)?;
+        cx.global()
+            .set(&mut cx, format!("query_{}", instance).as_ref(), module)?;
         Ok(())
     })
     .unwrap();
 }
 
-fn query(query: &'static str) {
+pub fn query(instance: &str, query: &'static str) -> QueryResult {
+    let instance = instance.to_owned();
     sync_node(move |mut cx| {
-        let script = cx.string("query.run");
+        let script = cx.string(format!("query_{}.run", instance));
         let func: Handle<JsFunction> = eval(&mut cx, script)?.downcast_or_throw(&mut cx)?;
         let undefined = cx.undefined();
         let query: Handle<JsValue> = cx.string(query).upcast();
         let cb: Handle<JsValue> = JsFunction::new(&mut cx, callback)?.upcast();
-        func.call(&mut cx, undefined, vec![query, cb])?;
-        Ok(())
+        let result = &**func
+            .call(&mut cx, undefined, vec![query, cb])?
+            .downcast_or_throw::<JsBox<QueryResult>, _>(&mut cx)?;
+        Ok(result.clone())
     })
-    .unwrap();
+    .unwrap()
 }
 
-fn callback(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+fn callback(mut cx: FunctionContext) -> JsResult<JsBox<QueryResult>> {
     let err: Handle<JsValue> = cx.argument(0)?;
     if err.is_a::<JsError, _>(&mut cx) {
-        println!(
-            "error: {}",
-            err.downcast_or_throw::<JsError, _>(&mut cx)?
-                .to_string(&mut cx)?
-                .value(&mut cx)
-        );
+        let err = err
+            .downcast_or_throw::<JsError, _>(&mut cx)?
+            .to_string(&mut cx)?
+            .value(&mut cx);
+        return Ok(cx.boxed(QueryResult::Err(err)));
     }
     let res = cx.argument_opt(1);
     if let Some(res) = res {
-        println!(
-            "result: {}",
-            res.downcast_or_throw::<JsString, _>(&mut cx)?
-                .value(&mut cx)
-        );
+        let ok = res
+            .downcast_or_throw::<JsString, _>(&mut cx)?
+            .value(&mut cx);
+        return Ok(cx.boxed(QueryResult::Ok(ok)));
     }
-    Ok(cx.undefined())
+    return Ok(cx.boxed(QueryResult::Err("No value".to_string())));
 }
 
-pub fn sync_node<T: Send + 'static>(
+fn sync_node<T: Send + 'static>(
     f: impl FnOnce(TaskContext) -> NeonResult<T> + Send + 'static,
 ) -> Option<T> {
     let nodejs_channel = nodejs::channel();
@@ -89,4 +81,15 @@ pub fn sync_node<T: Send + 'static>(
         Ok(())
     });
     rx.recv().ok()
+}
+
+pub(crate) fn remove(instance: &str) {
+    let instance = instance.to_owned();
+    sync_node(move |mut cx| {
+        let undefined = cx.undefined();
+        cx.global()
+            .set(&mut cx, format!("query_{}", instance).as_ref(), undefined)?;
+        Ok(())
+    })
+    .unwrap();
 }
